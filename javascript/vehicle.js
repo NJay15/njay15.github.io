8879e784e6e0eaa0f3075ecfe2998d4578988ddf
NJay15 <NJD15@gmx.de>
2018-09-01 15:11:07 +0200

    Update vehicle.js

--- javascript/vehicle.js
+++ javascript/vehicle.js
@@ -19,7 +19,7 @@ Vehicle.prototype.setTarget = function(x, y) {
 }
 
 Vehicle.prototype.behaviors = function() {
-  if(gravity){
+  if(params.gravity){
     this.setTarget(this.pos.x, height);
   }
   var arrive = this.arrive(this.target);
